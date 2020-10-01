e3af546361c6514c012d1683516a0b765ecb4873
David Mosquera <mosqueradavid@outlook.es>
2020-09-30 22:54:32 -0500

    [FEATURE], Add dynamic info to dropdowns

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,13 +4,11 @@ import RememberPassword from "../components/RememberPassword";
 import Navbar from "../components/Navbar";
 import ListSuperadmin from "../components/ListsSuperadmin";
 import Container from "../components/Container";
-import ModUserInfo from "../components/ModUserInfo";
-import UserInfo from "../components/UserInfo";
 import UserRegistration from "../components/UserRegistration";
 const App = () => {
   return (
     <>
-      <UserRegistration />
+      <Container />
     </>
   );
 };
