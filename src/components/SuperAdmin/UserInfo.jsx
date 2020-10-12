2ff8784a6436f71f7913b682fcf3b0991581e499
David Mosquera <mosqueradavid@outlook.es>
2020-10-12 11:03:38 -0500

    [CHANGES], Change UserInfo title & add container class

--- src/components/SuperAdmin/UserInfo.jsx
+++ src/components/SuperAdmin/UserInfo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Typography, Grid } from "@material-ui/core";
+import { Card, Typography, Grid, Box, Container } from "@material-ui/core";
 import {
   makeStyles,
   createMuiTheme,
@@ -35,6 +35,15 @@ const useStyles = makeStyles((theme) => ({
   fixmargin: {
     marginRight: "20px",
   },
+  box: {
+    marginBottom: theme.spacing(5),
+    textAlign: "center",
+  },
+  container: {
+    backgroundColor: "#cfe8fc",
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+  },
 }));
 
 const UserInfo = () => {
@@ -42,157 +51,169 @@ const UserInfo = () => {
 
   return (
     <>
-      <div className={classes.root}>
-        <Card raised className={classes.card}>
-          <Grid container spacing={2}>
-            <Grid item xs={6} sm={6} md={6}>
-              <ThemeProvider theme={theme}>
+      <Container className={classes.container}>
+        <div className={classes.root}>
+          <Card raised className={classes.card}>
+            <Box className={classes.box}>
+              <Typography
+                variant="h4"
+                color="primary"
+                gutterBottom
+                className={classes.tipography}
+              >
+                Información del usuario
+              </Typography>
+            </Box>
+            <Grid container spacing={2}>
+              <Grid item xs={6} sm={6} md={6}>
+                <ThemeProvider theme={theme}>
+                  <Typography
+                    variant="h5"
+                    color="primary"
+                    gutterBottom
+                    className={classes.typography}
+                  >
+                    Nombre
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    color="initial"
+                    className={classes.userInfo}
+                  >
+                    Lennin Geovany Ibarra Gonzáles
+                  </Typography>
+                </ThemeProvider>
+              </Grid>
+
+              <Grid item xs={6} sm={6} md={6}>
                 <Typography
                   variant="h5"
                   color="primary"
                   gutterBottom
                   className={classes.typography}
                 >
-                  Nombre
+                  Organización
                 </Typography>
                 <Typography
                   variant="h5"
                   color="initial"
                   className={classes.userInfo}
                 >
-                  Lennin Geovany Ibarra Gonzáles
+                  Tierra de Fuego
                 </Typography>
-              </ThemeProvider>
-            </Grid>
+              </Grid>
 
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Organización
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                Tierra de Fuego
-              </Typography>
-            </Grid>
+              <Grid item xs={6} sm={6} md={6}>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  gutterBottom
+                  className={classes.typography}
+                >
+                  Tipo de Documento
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  className={classes.userInfo}
+                >
+                  Cédula
+                </Typography>
+              </Grid>
 
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Tipo de Documento
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                Cédula
-              </Typography>
-            </Grid>
+              <Grid item xs={6} sm={6} md={6}>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  gutterBottom
+                  className={classes.typography}
+                >
+                  Número
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  className={classes.userInfo}
+                >
+                  1307111206
+                </Typography>
+              </Grid>
 
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Número
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                1307111206
-              </Typography>
-            </Grid>
+              <Grid item xs={6} sm={6} md={6}>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  gutterBottom
+                  className={classes.typography}
+                >
+                  Correo Electrónico
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  className={classes.userInfo}
+                >
+                  gerencia@allnexus.com.co
+                </Typography>
+              </Grid>
 
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Correo Electrónico
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                gerencia@allnexus.com.co
-              </Typography>
-            </Grid>
+              <Grid item xs={6} sm={6} md={6}>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  gutterBottom
+                  className={classes.typography}
+                >
+                  Celular
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  className={classes.userInfo}
+                >
+                  315 441 5598
+                </Typography>
+              </Grid>
 
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Celular
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                315 441 5598
-              </Typography>
-            </Grid>
+              <Grid item xs={6} sm={6} md={6}>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  gutterBottom
+                  className={classes.typography}
+                >
+                  Estado
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  className={classes.userInfo}
+                >
+                  Activo
+                </Typography>
+              </Grid>
 
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Estado
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                Activo
-              </Typography>
+              <Grid item xs={6} sm={6} md={6}>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  gutterBottom
+                  className={classes.typography}
+                >
+                  Rol
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  className={classes.userInfo}
+                >
+                  Administrador
+                </Typography>
+              </Grid>
             </Grid>
-
-            <Grid item xs={6} sm={6} md={6}>
-              <Typography
-                variant="h5"
-                color="primary"
-                gutterBottom
-                className={classes.typography}
-              >
-                Rol
-              </Typography>
-              <Typography
-                variant="h5"
-                color="initial"
-                className={classes.userInfo}
-              >
-                Administrador
-              </Typography>
-            </Grid>
-          </Grid>
-        </Card>
-      </div>
+          </Card>
+        </div>
+      </Container>
     </>
   );
 };
